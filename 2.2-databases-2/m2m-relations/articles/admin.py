123364b17fc77b16bf9b06cfa82d46cee19233ac
Igor Vazhenin <vidok0577@gmail.com>
2023-12-14 22:11:27 +0500

    2.2 in process

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import Article
 
 
+class
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,21 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    name = models.CharField(max_length=100)
+    articles = models.ManyToManyField(Article, related_name='articles', through='ArticleScopes')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleScopes(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
